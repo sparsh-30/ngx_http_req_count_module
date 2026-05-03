e16175898c41fe1f5dcd724c6d9da7c5b309168d
Sparsh Gupta <gsparsh04@gmail.com>
2026-05-03 13:03:58 +0000

    Add definition and parsing function for count_req directive
    We have defined the 'count_req' directive in nginx directives
    variable and also, completed the parsing function for it. This
    also includes the functions to create and merge location configs.

--- ngx_http_req_count_module.c
+++ ngx_http_req_count_module.c
@@ -10,17 +10,27 @@ typedef struct {
 } ngx_http_req_count_shm_ctx;
 
 typedef struct {
-    ngx_array_t count_zones;
+    ngx_array_t *count_zones;
 } ngx_http_req_count_conf_t;
 
 static char *ngx_http_req_count_zone(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_req_count_init_zone(ngx_shm_zone_t *shm_zone, void *data);
+static char *ngx_http_req_count(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static void *ngx_http_req_count_create_conf(ngx_conf_t *cf);
+static char *ngx_http_req_count_merge_conf(ngx_conf_t *cf, void *parent, void *child);
 
 static ngx_command_t ngx_http_req_count_commands[] = {
     { ngx_string("count_zone"),
       NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE2,
       ngx_http_req_count_zone,
       0,
+      0,
+      NULL },
+
+      { ngx_string("count_req"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_http_req_count,
+      NGX_HTTP_LOC_CONF_OFFSET,
       0,
       NULL },
     ngx_null_command
@@ -33,8 +43,8 @@ static ngx_http_module_t ngx_http_req_count_module_ctx = {
     NULL,
     NULL,
     NULL,
-    NULL,
-    NULL
+    ngx_http_req_count_create_conf,
+    ngx_http_req_count_merge_conf
 };
 
 ngx_module_t ngx_http_req_count_module = {
@@ -143,7 +153,8 @@ ngx_http_req_count_zone(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     ctx->zone_name = name;
 
     // ---- Create shared memory zone
-    shm_zone = ngx_shared_memory_add(cf, &name, ngx_pagesize, cmd->post);
+    shm_zone = ngx_shared_memory_add(cf, &name, ngx_pagesize,
+                                 &ngx_http_req_count_module);
     if (shm_zone == NULL) {
         return NGX_CONF_ERROR;
     }
@@ -196,4 +207,100 @@ ngx_http_req_count_init_zone(ngx_shm_zone_t *shm_zone, void *data)
     shm_zone->data = ctx;
 
     return NGX_OK;
+}
+
+static char *
+ngx_http_req_count(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_req_count_conf_t   *rcf = conf;
+
+    ngx_str_t                   *value;
+    ngx_str_t                    name = ngx_null_string;
+
+    ngx_shm_zone_t              *shm_zone;
+
+    value = cf->args->elts;
+
+    // Parse: count_req name=abc;
+    for (ngx_uint_t i = 1; i < cf->args->nelts; i++) {
+
+        if (ngx_strncmp(value[i].data, "name=", 5) == 0) {
+            name.len = value[i].len - 5;
+            name.data = value[i].data + 5;
+            continue;
+        }
+
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "invalid parameter \"%V\"", &value[i]);
+        return NGX_CONF_ERROR;
+    }
+
+    if (name.len == 0) {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "count_req: \"name\" is required");
+        return NGX_CONF_ERROR;
+    }
+
+    // Lookup existing shared memory zone
+    shm_zone = ngx_shared_memory_add(cf, &name, 0, &ngx_http_req_count_module);
+    if (shm_zone == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    if (shm_zone->data == NULL) {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "unknown count_zone \"%V\"", &name);
+        return NGX_CONF_ERROR;
+    }
+
+    // Initialize array if needed
+    if (rcf->count_zones == NULL) {
+        rcf->count_zones = ngx_array_create(cf->pool, 1,
+                                            sizeof(ngx_shm_zone_t *));
+        if (rcf->count_zones == NULL) {
+            return NGX_CONF_ERROR;
+        }
+    }
+
+    // Push zone into array
+    ngx_shm_zone_t **zone;
+
+    zone = ngx_array_push(rcf->count_zones);
+    if (zone == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    *zone = shm_zone;
+
+    return NGX_CONF_OK;
+}
+
+static void *
+ngx_http_req_count_create_conf(ngx_conf_t *cf)
+{
+    ngx_http_req_count_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_req_count_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    // Important: initialize to NULL so merge logic works correctly
+    conf->count_zones = NULL;
+
+    return conf;
+}
+
+static char *
+ngx_http_req_count_merge_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_http_req_count_conf_t *prev = parent;
+    ngx_http_req_count_conf_t *conf = child;
+
+    // If child doesn't define zones → inherit from parent
+    if (conf->count_zones == NULL) {
+        conf->count_zones = prev->count_zones;
+    }
+
+    return NGX_CONF_OK;
 }
